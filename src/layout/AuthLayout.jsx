d002497ada7b52e5faa8ef55724de5526a79177f
diegovr <diego.vr.dw@gmail.com>
2023-09-07 20:58:39 +0200

    modificación layout páginas de autenticación

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -3,7 +3,6 @@ import { Outlet } from 'react-router-dom';
 const AuthLayout = () => {
   return (
     <>
-      <h1>Desde Auth Layout</h1>
       <main className="container mx-auto md:grid md:grid-cols-2 mt-10 gap-12 p-5 items-center">
         <Outlet />
       </main>
